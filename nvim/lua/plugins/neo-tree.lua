05475d707b5f9756109b59f4aa4b068d4b8a68b8
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-09-09 10:26:55 +0200

    Show hidden files by default

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,13 @@ return {
     require("neo-tree").setup({
       -- follow your cwd with WD
       follow = true,
+      filesystem = { 
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        }
+      }
     })
   end,
 }
