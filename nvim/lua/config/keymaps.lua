039e2e83bb8a2aa5dc7d93430e11215404afceb6
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2025-01-30 08:38:56 +0100

    Fix keymaps manually

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 -- leader+w to close buffer without closing the window
 vim.api.nvim_set_keymap("n", "<leader>w", ":bp|bd#<CR>", { noremap = true, silent = true })
+
+-- telescope
+vim.keymap.set("n", "<leader><leader>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>sg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
